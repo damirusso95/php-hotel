bb0596a800e3286e2011e7e3c7f247296370b93b
damirusso95 <damirusso95@gmail.com>
2024-06-11 14:22:07 +0200

    first commit: stampo in pagina tutto l'array usando il foreach

---
+++ hotel.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Elenco Hotel</title>
+</head>
+<body>
+    <?php
+    $hotels = [
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+    ];
+            // itero sull'array
+    foreach ($hotels as $hotel) {
+        echo '<h2>' . $hotel['name'] . '</h2>';
+        echo '<p>' . $hotel['description'] . '</p>';
+        echo '<p>Parking: ' . ($hotel['parking'] ? 'Yes' : 'No') . '</p>';
+        echo '<p>Vote: ' . $hotel['vote'] . '</p>';
+        echo '<p>Distance to center: ' . $hotel['distance_to_center'] . ' km</p>';
+        echo '<hr>';
+    }
+    ?>
+</body>
+</html>
